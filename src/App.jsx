195a4f250ee05c86df679aa96b418fc8d014ded1
VICTOR MERSEGUEL <merseguel@gmail.com>
2024-02-02 08:27:22 +0000

    teste render

--- src/App.jsx
+++ src/App.jsx
@@ -9,12 +9,13 @@ function App() {
   let { globalLanguage } = useContext(GlobalContext);
 
   return (
-    <Routes>
-      <Route index element={<CompletePage lang={globalLanguage} />} />
-      <Route path="/fr" element={<CompletePage lang="fr" />} />
-      <Route path="/pt" element={<CompletePage lang="pt" />} />
-      <Route path="/en" element={<CompletePage lang="en" />} />
-    </Routes>
+    // <Routes>
+    //   <Route index element={<CompletePage lang={globalLanguage} />} />
+    //   <Route path="/fr" element={<CompletePage lang="fr" />} />
+    //   <Route path="/pt" element={<CompletePage lang="pt" />} />
+    //   <Route path="/en" element={<CompletePage lang="en" />} />
+    // </Routes>
+    <div>Teste</div>
   );
 }
 
